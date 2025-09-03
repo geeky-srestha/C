11b50be6030a3abc9eef402175672ae5fa413ed8
Shrestha K. <srjtti30@gmail.com>
2025-09-03 19:46:13 +0530

    greatest of 3 numbers
    C program to find greatest of 3 numbers.

---
+++ Ex2/greatest3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+
+printf("Enter three numbers");
+scanf("%d %d %d", &a,&b,&c);
+
+if(a>b)
+{
+ if(a>c)
+   printf("A is Greatest");
+ else
+   printf("C is Greatest");
+}
+else
+{
+  if(b>c)
+    printf("B is Greatest");
+  else
+    printf("C is Greatest");
+}
+}
