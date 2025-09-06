f9551950edde1bbabba86db94e37b6c42d28cfbd
Shrestha K. <srjtti30@gmail.com>
2025-09-06 19:39:56 +0530

    Create Triangle_Pattern2.c

---
+++ Pattern Programs/Triangle_Pattern2.c
@@ -0,0 +1,17 @@
+// C program to print triangle pattern
+
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter the value of n");
+scanf("%d", &n);     // takes value of n form the user
+for(int i=n;i>=1;i--)
+{
+  for(int j=1;j<=i;j++)
+  {
+    printf(" %d", j);
+  }
+  printf("\n");     // for new line(row)
+}
+}
