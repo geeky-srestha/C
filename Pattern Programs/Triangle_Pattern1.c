1a52010117403024c2f405658460025431b064dd
Shrestha K. <srjtti30@gmail.com>
2025-09-06 19:35:50 +0530

    Create Triangle_Pattern1.c
    to print triangle pattern

---
+++ Pattern Programs/Triangle_Pattern1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter the value of n");
+scanf("%d", &n);     // takes value of n form the user
+for(int i=1;i<=n;i++)
+{
+  for(int j=1;j<=i;j++)
+  {
+    printf(" %d", j);
+  }
+  printf("\n");     // for new line(row)
+}
+}
